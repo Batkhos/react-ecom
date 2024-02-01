c74bf8e86176d12c83d604657d3aaf3950a239f0
Adachi <azaoui156@gmail.com>
2024-02-01 22:02:51 +0100

    test dah

---
+++ src/products.js
@@ -0,0 +1,33 @@
+import product1 from "./assets/iPhone15.jpg"
+import product2 from "./assets/macbook.jpeg"
+import product3 from "./assets/airpods.jpg"
+import product4 from "./assets/watch.jpg"
+
+
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "IPhone",
+        price: 999.00,
+        productImage: product1
+    },
+    {
+        id: 2,
+        productName: "MacBook",
+        price: 1999.00,
+        productImage: product2
+    },
+    {
+        id: 3,
+        productName: "AirPods",
+        price: 249.00,
+        productImage: product3
+    },
+    {
+        id: 4,
+        productName: "Watch",
+        price: 549.00,
+        productImage: product4
+    }
+]
